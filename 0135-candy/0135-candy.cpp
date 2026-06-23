93b763eafe5713066335255b2e84cf4dca7a36b3
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-23 23:05:10 +0530

    Time: 0 ms (100%), Space: 23.6 MB (11.3%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
+    //Approach1 : using left and right array
+    //T.C : O(3n)
+    //S.C :O(2n)
     int candy(vector<int>& ratings) {
-        
         int n = ratings.size();
-        int candies = n;
 
-        int i = 1;
-        while(i < n){
-            //if same rating
-            if(ratings[i] == ratings[i-1]){
-                i++;
-                continue;
+        vector<int> left(n , 0);
+        vector<int> right(n , 0);
+
+        //left neighbours
+        left[0] = 1;
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1]+1;
+            }else{
+                left[i] = 1;
             }
-
-            //increasing slope
-            int peak = 0;
-            while(ratings[i] > ratings[i-1]){
-                peak++;
-                candies += peak;
-                i++;
-
-                if(i == n) return candies;
-            }
-
-            //decreasing slope
-            int dip = 0;
-            while(i < n && ratings[i] < ratings[i-1]){
-                dip++;
-                candies += dip;
-                i++;
-            }
-
-            //after completing mountain
-            candies -= min(peak , dip);
         }
-        
-        return candies;
-        //T.C : O(N)
-        //S.C : O(1)
+
+        //right neighbours
+        right[n-1] = 1;
+        for(int i = n-2; i >= 0; i--){
+            if(ratings[i] > ratings[i+1]){
+                right[i] = right[i+1]+1;
+            }else{
+                right[i] = 1;
+            }
+        }
+
+        int total = 0;
+
+        for(int i = 0; i < n; i++){
+            total += max(left[i] , right[i]);
+        }
+
+        return total;
     }
 };
