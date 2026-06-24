6202fd8ad93f16fbbd8b465f987c16a00535bc6f
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-24 14:33:31 +0530

    Time: 10 ms (45.58%), Space: 7.7 MB (64.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    //Approach1 : using loop
     int fib(int n) {
-        int a = 0 , b = 1 , c;
-        int count = 2;
-        if(n<=1) return n;
-        while(count <= n){
-            c = a+b;
-            a = b;
-            b = c;
-            count++;
-        }
-        return c;
+        if(n <= 1) return n;
+
+        int last = fib(n-1);
+        int slast = fib(n-2);
+
+        return last+ slast;
     }
 };
