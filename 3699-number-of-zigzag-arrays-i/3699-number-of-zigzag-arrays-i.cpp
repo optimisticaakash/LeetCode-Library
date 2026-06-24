555c800a2c0792fa19043661a57c274604006c75
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-25 00:49:26 +0530

    Update 3699-number-of-zigzag-arrays-i.cpp

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,3 +1,109 @@
+//Approach1: recursion
+class Solution {
+public: 
+    int Mod = 1e9+7;
+    typedef long long ll;
+    int N , M;
+    int solve(int i , int prevVal , bool increasing){
+
+        if(i == N){
+            //we reached the end of zig zag array
+            return 1;
+        }
+
+        ll result = 0;
+
+        if(increasing == true){
+            for(int nextVal = prevVal + 1; nextVal <=  M; nextVal++){
+                result = (result + solve(i + 1 , nextVal , false)) % Mod;
+            }
+        }else{//decreasing
+            for(int nextVal = 1; nextVal <  prevVal; nextVal++){
+                result = (result + solve(i + 1 , nextVal , true)) % Mod;
+            }
+        }
+
+        return result;
+    }
+    int zigZagArrays(int n, int l, int r) {
+        N = n;
+        M = r - l +1;
+        l = 1;
+        r = M;
+
+        ll result = 0;
+
+        for(int startVal = 1; startVal <= M; startVal++){
+            //i = 0 pr startVal dal diya hai
+
+            //increasing
+            result = (result + solve(1 , startVal  , true)) % Mod;
+
+            //decreasing
+            result = (result + solve(1 , startVal  , false)) % Mod;
+
+        }
+        return result;
+    }
+};
+
+//Approach2: recursion+ memoization
+class Solution {
+public: 
+    int Mod = 1e9+7;
+    typedef long long ll;
+    int N , M;
+
+    int t[2001][2001][2];
+    int solve(int i , int prevVal , bool increasing){
+
+        if(i == N){
+            //we reached the end of zig zag array
+            return 1;
+        }
+
+        if(t[i][prevVal][increasing] != -1){
+            return t[i][prevVal][increasing];
+        }
+
+        ll result = 0;
+
+        if(increasing == true){
+            for(int nextVal = prevVal + 1; nextVal <=  M; nextVal++){
+                result = (result + solve(i + 1 , nextVal , false)) % Mod;
+            }
+        }else{//decreasing
+            for(int nextVal = 1; nextVal <  prevVal; nextVal++){
+                result = (result + solve(i + 1 , nextVal , true)) % Mod;
+            }
+        }
+
+        return t[i][prevVal][increasing] = result;
+    }
+    int zigZagArrays(int n, int l, int r) {
+        memset(t , -1 , sizeof(t));
+        N = n;
+        M = r - l +1;
+        l = 1;
+        r = M;
+
+        ll result = 0;
+
+        for(int startVal = 1; startVal <= M; startVal++){
+            //i = 0 pr startVal dal diya hai
+
+            //increasing
+            result = (result + solve(1 , startVal  , true)) % Mod;
+
+            //decreasing
+            result = (result + solve(1 , startVal  , false)) % Mod;
+
+        }
+        return result;
+    }
+};
+
+//Approach3: dp + prefixSum
 class Solution {
 public:
     int MOD = 1e9 + 7;
