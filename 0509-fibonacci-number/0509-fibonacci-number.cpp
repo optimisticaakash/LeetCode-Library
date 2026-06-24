cff1e203bc75e048c6398b6835fa2077494a7cb9
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-24 14:58:46 +0530

    Update 0509-fibonacci-number.cpp

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -49,3 +49,26 @@ public:
         return last+ slast;
     }
 };
+
+///Approach4: using memoization
+class Solution {
+public:
+    int solve(int n , vector<int>& dp){
+        if(n <= 1) return n;
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = solve(n-1 , dp) + solve(n-2 , dp);
+    }
+    int fib(int n) {
+        if(n <= 1) return n;
+
+        vector<int> dp(n+1 , -1);
+
+        return solve(n , dp);
+    }
+};
+//T.C : O(N)
+//S.C : 
