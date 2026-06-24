22295375a63121a20f916f119e261228d7f476d7
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-24 14:41:48 +0530

    Update 0509-fibonacci-number.cpp

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,3 +1,43 @@
+//Approach1 : using loop
+class Solution {
+public:
+    
+    int fib(int n) {
+        int a = 0 , b = 1 , c;
+        int count = 2;
+        if(n<=1) return n;
+        while(count <= n){
+            c = a+b;
+            a = b;
+            b = c;
+            count++;
+        }
+        return c;
+    }
+};
+//T.C : O(N)
+//S.C : O(1)
+
+//Approach2:using vector
+class Solution {
+public:
+    int fib(int n) {
+        if(n== 0) return 0;
+        vector<int>fibo(n+1);
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for(int i = 2; i <=n; i++){
+            fibo[i] = fibo[i-1]+fibo[i-2];
+        }
+
+        return fibo[n];
+    }
+};
+//T.C : O(N)
+//S.C :O(N)
+
+//Approach3 : using recursion
 class Solution {
 public:
     int fib(int n) {
