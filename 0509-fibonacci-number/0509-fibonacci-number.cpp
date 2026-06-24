767d512ba3e32965157561137ee2dabe7910b738
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-24 15:01:35 +0530

    Time: 3 ms (65.04%), Space: 8.2 MB (16.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,74 +1,17 @@
-//Approach1 : using loop
 class Solution {
 public:
     
     int fib(int n) {
-        int a = 0 , b = 1 , c;
-        int count = 2;
-        if(n<=1) return n;
-        while(count <= n){
-            c = a+b;
-            a = b;
-            b = c;
-            count++;
-        }
-        return c;
-    }
-};
-//T.C : O(N)
-//S.C : O(1)
-
-//Approach2:using vector
-class Solution {
-public:
-    int fib(int n) {
-        if(n== 0) return 0;
-        vector<int>fibo(n+1);
-        fibo[0] = 0;
-        fibo[1] = 1;
-
-        for(int i = 2; i <=n; i++){
-            fibo[i] = fibo[i-1]+fibo[i-2];
-        }
-
-        return fibo[n];
-    }
-};
-//T.C : O(N)
-//S.C :O(N)
-
-//Approach3 : using recursion
-class Solution {
-public:
-    int fib(int n) {
-        if(n <= 1) return n;
-
-        int last = fib(n-1);
-        int slast = fib(n-2);
-
-        return last+ slast;
-    }
-};
-
-///Approach4: using memoization
-class Solution {
-public:
-    int solve(int n , vector<int>& dp){
-        if(n <= 1) return n;
-
-        if(dp[n] != -1){
-            return dp[n];
-        }
-
-        return dp[n] = solve(n-1 , dp) + solve(n-2 , dp);
-    }
-    int fib(int n) {
         if(n <= 1) return n;
 
         vector<int> dp(n+1 , -1);
+        dp[0] = 0;
+        dp[1] = 1;
 
-        return solve(n , dp);
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1]+ dp[i-2];
+        }
+
+        return dp[n];
     }
 };
-//T.C : O(N)
-//S.C : 
