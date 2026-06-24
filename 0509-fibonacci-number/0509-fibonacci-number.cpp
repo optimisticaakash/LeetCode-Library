3939b267cadf04494175b7a005769e203dd95548
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-24 15:04:36 +0530

    Update 0509-fibonacci-number.cpp

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,3 +1,62 @@
+//Approach1 : using loop
+class Solution {
+public:
+    
+    int fib(int n) {
+        int a = 0 , b = 1 , c;
+        int count = 2;
+        if(n<=1) return n;
+        while(count <= n){
+            c = a+b;
+            a = b;
+            b = c;
+            count++;
+        }
+        return c;
+    }
+};
+//T.C : O(N)
+//S.C : O(1)
+
+//Approach2 : using recursion
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) return n;
+
+        int last = fib(n-1);
+        int slast = fib(n-2);
+
+        return last+ slast;
+    }
+};
+//T.C : O(2^N)
+//S.C : O(N)
+
+///Approach3: using memoization
+class Solution {
+public:
+    int solve(int n , vector<int>& dp){
+        if(n <= 1) return n;
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = solve(n-1 , dp) + solve(n-2 , dp);
+    }
+    int fib(int n) {
+        if(n <= 1) return n;
+
+        vector<int> dp(n+1 , -1);
+
+        return solve(n , dp);
+    }
+};
+//T.C : O(N)
+//S.C : O(N)
+
+//Approach4: bottom up 
 class Solution {
 public:
     
@@ -15,3 +74,5 @@ public:
         return dp[n];
     }
 };
+//T.C : O(N)
+//S.C :O(N)
