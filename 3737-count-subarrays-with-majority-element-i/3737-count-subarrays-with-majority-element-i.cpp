573804c35af061c0afce589221ea75d5dcb65f7b
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-25 21:26:49 +0530

    Time: 48 ms (44.88%), Space: 39.6 MB (75.6%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,29 +1,3 @@
-//Approach1 : bruteforce 
-class Solution {
-public:
-    int countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int cnt = 0;
-
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                int freq = 0;
-                for(int k = i; k <= j; k++){
-                    if(nums[k] == target) freq++;
-                }
-
-                if(freq > (j-i+1)/2) cnt++;
-            }
-        }
-
-        return cnt;
-    }
-};
-//T.C : O(N^3)
-//S.C : O(1) -> it will give tle
-
-
-//Approach2 : better brute -> calculate on the fly 
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
@@ -33,9 +7,13 @@ public:
         for(int i = 0; i < n; i++){
             int freq = 0;
             for(int j = i; j < n; j++){
-                if(nums[j] == target) freq++;
+                if(nums[j] == target){
+                    freq++;
+                }else{
+                    freq--;
+                }
 
-                if(freq > (j-i+1)/2){
+                if(freq > 0){
                     cnt++;
                 }
             }
@@ -44,5 +22,3 @@ public:
         return cnt;
     }
 };
-//T.C : O(N^2)
-//S.C : O(N)
