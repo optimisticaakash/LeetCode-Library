6193709be0e41c60daaa0f091ea0df12d70c0a25
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-25 22:09:07 +0530

    Update 3737-count-subarrays-with-majority-element-i.cpp

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,3 +1,80 @@
+//Approach1: Bruteforce
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int freq = 0;
+                for(int k = i; k <= j; k++){
+                    if(nums[k] == target) freq++;
+                }
+                if(freq > (j-i+1)/2){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
+//T.C : O(N^3)
+//S.C : O(1)
+
+//Approach2: Better Bruteforce -> calculate count on the fly
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            int freq = 0;
+            for(int j = i; j < n; j++){
+                if(nums[j] == target) freq++;
+
+                if(freq > (j-i+1)/2){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
+//T.C : O(N^2)
+//S.C : O(1)
+
+//Approach3: simplified bruteforce -> divide operation is costly so avoiding it
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            int freq = 0;
+            for(int j = i; j < n; j++){
+                if(nums[j] == target){
+                    freq++;
+                }else{
+                    freq--;
+                }
+
+                if(freq > 0){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
+//T.C : O(N^2)
+//S.C : O(1)
+
 //Approach4:Cummulative Sum/ prefix Sum
 class Solution {
 public:
@@ -30,5 +107,5 @@ public:
         return cnt;
     }
 };
-//T.C : O(N^2)
-//S.C : O(1)
+//T.C : O(N^2) + O(N)
+//S.C : O(N)
