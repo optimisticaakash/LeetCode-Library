051709e6960694af493c162439d00c2e4767de4f
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-25 21:20:23 +0530

    Update 3737-count-subarrays-with-majority-element-i.cpp

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,3 +1,29 @@
+//Approach1 : bruteforce 
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int freq = 0;
+                for(int k = i; k <= j; k++){
+                    if(nums[k] == target) freq++;
+                }
+
+                if(freq > (j-i+1)/2) cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
+//T.C : O(N^3)
+//S.C : O(1) -> it will give tle
+
+
+//Approach2 : better brute -> calculate on the fly 
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
@@ -18,3 +44,5 @@ public:
         return cnt;
     }
 };
+//T.C : O(N^2)
+//S.C : O(N)
