c26ba19e59c855f19c1263059dd187ca22e753ef
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-25 21:28:23 +0530

    Update 3737-count-subarrays-with-majority-element-i.cpp

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,3 +1,53 @@
+//Approach1 : bruteforce 
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int freq = 0;
+                for(int k = i; k <= j; k++){
+                    if(nums[k] == target) freq++;
+                }
+
+                if(freq > (j-i+1)/2) cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};
+//T.C : O(N^3)
+//S.C : O(1) -> it will give tle
+
+
+//Approach2 : better brute -> calculate on the fly 
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            int freq = 0;
+            for(int j = i; j < n; j++){
+                if(nums[j] == target) freq++;
+
+                if(freq > (j-i+1)/2){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
+//T.C : O(N^2)
+//S.C : O(N)
+
+//Approach3:simplified bruteforce : since division is costly we are avoid doing it
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
@@ -22,3 +72,6 @@ public:
         return cnt;
     }
 };
+//T.C : O(N^2)
+//S.C : O(N)
+
