ffaa39511e362320eec6ffd6751675fb89372ffe
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-26 21:43:04 +0530

    Update 0198-house-robber.cpp

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,3 +1,54 @@
+//Approach1: recursion
+class Solution {
+public:
+    int solve(vector<int>& nums , int i , int n){
+
+        //base case
+        if(i >= n) return 0;
+
+        //we have only two option
+        int steal = nums[i] + solve(nums , i + 2 , n);
+
+        int skip = solve(nums , i+1 , n);
+
+        return max(steal , skip);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        return solve(nums , 0 , n);
+    }
+};
+//T.C : O(2^n)
+//S.C : O(n)
+
+//Approach2: recursion + memoization
+class Solution {
+public:
+    int t[101];
+    int solve(vector<int>& nums , int i , int n){
+
+        //base case
+        if(i >= n) return 0;
+
+        if(t[i] != -1) return t[i];
+
+        //we have only two option
+        int steal = nums[i] + solve(nums , i + 2 , n);
+
+        int skip = solve(nums , i+1 , n);
+
+        return t[i] = max(steal , skip);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(nums , 0 , n);
+    }
+};
+//T.C : O(N)
+//S.C : O(N)
+//Approach3 : bottom up - approach
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -19,3 +70,5 @@ public:
         return t[n];
     }
 };
+//T.C : O(N)
+//S.C : O(N)
