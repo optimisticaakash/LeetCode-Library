589e30c372472786eb9de413bca0cb2036936b2e
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-26 14:45:48 +0530

    Time: 0 ms (100%), Space: 8.4 MB (52.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int t[46];
-    int solve(int n){
-        if(n < 0) return 0;
-
-        if(t[n] != -1) return t[n];
-
-        if(n == 0) return 1;//we have got 1 way to cover all stairs
-
-        int one_step = solve(n-1);
-        int two_step = solve(n-2);
-        
-        return t[n] =  one_step + two_step;
-    }
     int climbStairs(int n) {
-        memset(t,-1,sizeof(t));
-        return solve(n);
+        if(n ==1 || n == 2 || n ==3) return n;
+
+        vector<int> arr(n+1);
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
     }
 };
