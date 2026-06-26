fa6e78645dab9553a1f83ae480f0f553ad8b31eb
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-26 21:40:10 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int>& nums , int i , int n){
-
-        //base case
-        if(i >= n) return 0;
-
-        if(t[i] != -1) return t[i];
-
-        //we have only two option
-        int steal = nums[i] + solve(nums , i + 2 , n);
-
-        int skip = solve(nums , i+1 , n);
-
-        return t[i] = max(steal , skip);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        memset(t,-1,sizeof(t));
-        return solve(nums , 0 , n);
+
+        vector<int> t(n+1 , 0);
+        //t[i] = max stolen money till i house
+
+        t[0] = 0; //no house
+        t[1] = nums[0];//only 1 house
+
+        for(int i = 2; i<= n; i++){
+            int steal = nums[i-1] + t[i-2];
+            int skip = t[i-1];
+
+            t[i] = max(steal , skip);
+        }
+
+        return t[n];
     }
 };
