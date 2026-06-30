c2b84b9ae56caf9331dff3b30b133fa961a8a672
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-06-30 22:32:09 +0530

    Update 1358-number-of-substrings-containing-all-three-characters.cpp

--- LeetCode/Medium/1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ LeetCode/Medium/1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,18 +1,57 @@
+//Approach1. bruteforce
 class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.size();
 
-        vector<int> hash(3,-1);
-        int cnt = 0;
+        //generate all substring and check count
 
+        int cnt = 0;
         for(int i = 0; i < n; i++){
-            char ch = s[i];
-            hash[ch-'a'] = i;
-            if(hash[0]!= -1 && hash[1]!= -1 && hash[2]!= -1){
-                cnt += 1+min(hash[0],min(hash[1],hash[2]));
+            unordered_set<char> st;
+            for(int j = i; j < n; j++){
+                st.insert(s[j]);
+
+                if(st.size() == 3) cnt++;
             }
+
+            
         }
+
         return cnt;
     }
 };
+
+//T.C : O(N^2)
+//S.C : O(1)
+
+//Approach2 : using sliding window
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+
+        vector<int> mp(3,0);
+        int i = 0;
+        int j = 0;
+
+        int result = 0;
+
+        while(j < n){
+            char ch = s[j];
+            mp[ch-'a']++;
+
+            while(mp[0] > 0 && mp[1] > 0 && mp[2] > 0){
+                result += n-j;
+
+                mp[s[i]-'a']--; 
+                i++;
+            }
+            j++;
+        }
+        return result;
+    }
+};
+
+//T.C : O(2n)
+//S.C : O(1)
