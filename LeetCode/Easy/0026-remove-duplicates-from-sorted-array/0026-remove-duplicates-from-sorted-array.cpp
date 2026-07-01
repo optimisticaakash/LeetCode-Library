f46f08c74ee1b8b25b53b7255a07b01ce6c3a2c5
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-01 10:59:34 +0530

    Update 0026-remove-duplicates-from-sorted-array.cpp

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,3 +1,52 @@
+//Approach1: bruteforce using set
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+
+        set<int> st;
+
+        for(int i = 0; i < n; i++){
+            st.insert(nums[i]);
+        }
+
+        int idx = 0;
+        for(int s:st){
+            nums[idx++] = s;
+        }
+
+        return idx;
+    }
+};
+//T.C : O(nlogn + n) //set insertion takes logn
+//S.C  :O(N)
+
+
+//Approach2: using two pointer - 
+//code1: using while loop
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+
+        int i = 0;
+        int j = 1;
+
+        while( j < n && i < n ){
+            if(nums[i] != nums[j]){
+                nums[i+1] = nums[j];
+                i++;
+            }
+            j++;
+        }
+
+        return i+1;
+    }
+};
+//T.C : O(n)
+//S.C : O(1)
+
+//code2: using for loop
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -14,3 +63,7 @@ public:
         return i+1;
     }
 };
+
+//T.C : O(n)
+//S.C : O(1)
+
