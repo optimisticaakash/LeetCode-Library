c127cab831647d387f585d6d8c5cb958ce934c49
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-01 10:57:00 +0530

    07-01-2026 0026-remove-duplicates-from-sorted-array Easy Array

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,17 +3,14 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
 
-        set<int> st;
-
-        for(int i = 0; i < n; i++){
-            st.insert(nums[i]);
+        int i = 0;
+        for(int j=1; j < n; j++){
+            if(nums[i] != nums[j]){
+                nums[i+1] = nums[j];
+                i++;
+            }
         }
 
-        int idx = 0;
-        for(int s:st){
-            nums[idx++] = s;
-        }
-
-        return idx;
+        return i+1;
     }
 };
