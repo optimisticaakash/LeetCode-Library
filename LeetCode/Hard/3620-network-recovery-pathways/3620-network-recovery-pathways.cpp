a2a89b0e47fddf02edb9cd5973f3a2e22fd68d15
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-03 18:34:11 +0530

    07-03-2026 3620-network-recovery-pathways Hard Array

---
+++ LeetCode/Hard/3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    typedef pair<long long,int> P;
+    bool check(int mid , int n , long long k , unordered_map<int,vector<pair<int,int>>>& adj){
+        //result[i] = minimum cost path from source to i node
+
+        vector<long long > result (n , LLONG_MAX);
+
+        //minheap for dijkstra
+        priority_queue<P, vector<P>  , greater<P>> pq;
+
+        result[0] = 0;
+        pq.push({0 , 0});
+
+        while(!pq.empty()){
+            long long d = pq.top().first;
+            int node = pq.top().second;
+
+            pq.pop();
+
+            //total cost from source to node = d
+            if(d > result[node])
+                continue;
+
+            if(d > k) return false;
+
+            if(node == n-1) return true;
+
+            for(auto &vec: adj[node]){
+                int ngbr = vec.first;
+                int cost = vec.second;
+
+                if(cost < mid) continue;
+
+                if(d+ cost < result[ngbr]){
+                    result[ngbr] = d + cost;
+
+                    pq.push({d+cost , ngbr});
+                }
+            }
+        }
+
+        return false;
+    }
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        int n = online.size();// no of nodes
+
+        //adjacency list // u -> {( v , cost) , (v' , cost)}
+        unordered_map<int , vector<pair<int,int>>> adj;
+
+        int low = INT_MAX;
+        int high = 0;
+     
+        for(auto &edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int cost = edge[2];
+
+            if(!online[u] || !online[v])
+                continue;
+            
+            adj[u].push_back({v,cost});
+
+            low = min(low , cost);
+            high = max(high , cost);
+        }
+        int answer = -1;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+
+            if(check(mid , n , k , adj)){
+                answer = mid;
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+
+        return answer;
+
+    }
+};
