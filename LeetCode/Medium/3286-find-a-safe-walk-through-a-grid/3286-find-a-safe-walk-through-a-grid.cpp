58f62858a41423d96a0918f74702b66c9f647ea9
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-03 13:19:15 +0530

    Update 3286-find-a-safe-walk-through-a-grid.cpp

--- LeetCode/Medium/3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ LeetCode/Medium/3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,3 +1,56 @@
+//Approach1: dfs for traversal of whole grid
+class Solution {
+public:
+    int n , m;
+    vector<vector<int>> best;//store krega maximum health kisi cell tk pahuchne ki 
+
+    vector<vector<int>> dir = {{-1 , 0} , {1 , 0} , {0 , -1} , {0 , 1}};
+    bool dfs(vector<vector<int>>& grid ,int i , int j , int health){
+        //outside
+        if(i < 0 || j < 0 || i >= n || j >= m) 
+            return false;
+
+        //unsafe hoga to health kam hojyegi wrna same rhegi
+        health -= grid[i][j];
+
+        if(health <= 0)
+            return false;
+        
+        if(i == n-1 && j == m-1) return true;
+
+        if(best[i][j] != -1 && best[i][j] >= health){
+            return false;
+        }
+
+        best[i][j] = health;
+
+        //explore all 4 dir
+        for(vector<int> &vec: dir){
+            int new_i = i + vec[0];
+            int new_j = j + vec[1];
+
+            if(dfs(grid , new_i , new_j , health)){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        n = grid.size();
+        m = grid[0].size();
+
+
+        best.assign(n , vector<int>(m , -1));
+
+        bool ans = dfs(grid ,0 , 0 , health);   
+
+        return ans;
+    }
+};
+//T.C : O(n*m*H)
+//S.C : O(n*m)
+
+//Approach2: Dijsktra algorithm
 class Solution {
 public:
     typedef pair<int,pair<int,int>> P;
@@ -49,3 +102,5 @@ public:
         return dist[n-1][m-1] < health;
     }
 };
+//T.C : O(E+Vlog(v)) = O(n*m * log(nm))
+//S.C : O(nm)
