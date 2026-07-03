5c759528aa8866ffe4331d8335afc089c25059e7
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-03 16:24:45 +0530

    Update 3286-find-a-safe-walk-through-a-grid.cpp

--- LeetCode/Medium/3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ LeetCode/Medium/3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -104,3 +104,51 @@ public:
 };
 //T.C : O(E+Vlog(v)) = O(n*m * log(nm))
 //S.C : O(nm)
+
+//Approach3: 0/1 bfs
+class Solution {
+public:
+    vector<vector<int>> directions = {{-1 , 0} , {1 , 0} , {0 , -1} , {0 , 1}};
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> result(n , vector<int> (m , INT_MAX));
+        //result[i][j] = min health to reach from (0 , 0) to (n-1 , m-1)
+
+        deque<pair<int,int>> dq;
+
+        result[0][0] = grid[0][0];
+        dq.push_front({0,0});
+
+        while(!dq.empty()){
+            auto [r,c] = dq.front();
+            dq.pop_front();
+
+            for(auto &dir: directions){
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if(nr < 0 || nc < 0 || nr > n-1 || nc > m-1)
+                    continue;
+                
+                if(result[r][c] + grid[nr][nc] < result[nr][nc]){
+                    result[nr][nc] = result[r][c] + grid[nr][nc];
+
+                    if(grid[nr][nc] == 0){
+                        dq.push_front({nr , nc});
+                    }else{
+                        dq.push_back({nr , nc});
+                    }
+                }
+            }
+        }
+
+        int x = result[n-1][m-1];
+
+        return health-x >= 1;
+
+    }
+};
+//T.C : O(nm)
+//S.C  : O(nm)
