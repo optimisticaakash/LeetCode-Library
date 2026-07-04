de2f6e048b9af821e6450b664377408a6305ac72
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-05 00:24:37 +0530

    07-05-2026 0213-house-robber-ii Medium Array

--- LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.cpp
+++ LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int>& nums , int i , int n){
-
-        //base case
-        if(i > n){
-            return 0;
-        }
-
-        if(t[i] != -1){
-            return t[i];
-        }
-
-        //we have only two ways
-        //steal
-        int steal = nums[i] + solve(nums , i+2 , n);
-        int skip = solve(nums , i+1 , n);
-
-        return t[i] = max(steal , skip);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
 
         if(n == 1) return nums[0];
 
-        if(n == 2){
-            return max(nums[0] , nums[1]);
+        vector<int> t(n+1);
+        //t[i] = money when have i houses
+
+        int result1;
+        t[0] = 0;
+        //case1 : take 1st house -> skip the last house
+        for(int i = 1; i <= n-1; i++){
+            int skip = t[i-1];
+            int steal = nums[i-1] + (i-2 >= 0 ?t[i-2]: 0);
+
+            t[i] = max(steal , skip);
         }
+        result1 = t[n-1];
 
-        memset(t , -1 , sizeof(t));
-        //case1. take 0th index house (1st house);
-        int take_0th_house = solve(nums , 0 , n-2);
+        t.clear();
 
-        memset(t , -1 , sizeof(t));
-        //case2. take 1st index house (2nd house);
-        int take_1st_house = solve(nums , 1 , n-1);
+        int result2;
+        t[0] = 0;
+        t[1] = 0;//skip first house
+        //case2: skip first house -> take the last house
+        for(int i = 2; i <= n; i++){
+            int skip = t[i-1];
+            int steal = nums[i-1] + (i-2 >= 0 ? t[i-2]: 0);
 
-        return max(take_0th_house , take_1st_house);
+            t[i] = max(steal , skip);
+        }
+        result2 = t[n];
+
+
+        return max(result1 , result2);
+
     }
 };
