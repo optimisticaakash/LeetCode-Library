0909688b471c62c47cf8b4c58d847c3ae5de7cde
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-04 22:49:53 +0530

    07-04-2026 2492-minimum-score-of-a-path-between-two-cities Medium Depth-First Search

---
+++ LeetCode/Medium/2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    typedef pair<int,int> P;
+    void solvebydfs(unordered_map<int,vector<P>> &adj , int node , vector<bool>& visited , int &result){
+
+        visited[node] = true;
+
+        for(auto& vec: adj[node]){
+            int v = vec.first;
+            int cost = vec.second;
+
+            result = min(result , cost);
+
+            if(!visited[v]){
+                solvebydfs(adj , v , visited , result);
+            }
+        }
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        
+        unordered_map<int,vector<P>> adj;
+
+        //making the adjacency list
+        for(auto& road:roads){
+            int u = road[0];
+            int v = road[1];
+            int dist = road[2];
+
+            adj[u].push_back({v , dist});
+            adj[v].push_back({u, dist});
+        } 
+
+        vector<bool>visited(n+1 ,false);
+
+        int result = INT_MAX;
+        solvebydfs(adj , 1 , visited ,  result);
+
+        return result;
+    }
+};
