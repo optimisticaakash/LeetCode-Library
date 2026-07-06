37cdaa337fbe78b5fe734613ae50e6b0c7db458a
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-06 11:51:47 +0530

    Update 1288-remove-covered-intervals.cpp

--- LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,3 +1,4 @@
+//Approach1: sorting 
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
@@ -29,3 +30,40 @@ public:
         return count;
     }
 };
+//T.C : O(nlogn) + O(n)
+//S.C : O(n)
+
+//Approach2: In const space
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+
+        auto lambda = [](vector<int>& vec1, vector<int>& vec2){
+            if(vec1[0] == vec2[0]){
+                return vec1[1] > vec2[1];
+            }
+
+            return vec1[0] < vec2[0];
+        };
+
+        sort(begin(intervals) , end(intervals) , lambda);
+
+        int lastIntervalKaEnd = intervals[0][1];
+        int count = 1;
+
+    
+        for(int i = 1; i < n; i++){
+            if( lastIntervalKaEnd >= intervals[i][1]){
+                continue;
+            }
+
+           lastIntervalKaEnd =  intervals[i][1];
+           count++;
+        }
+
+        return count;
+    }
+};
+//T.C : O(nlogn) + O(n)
+//s.C : O(1)
