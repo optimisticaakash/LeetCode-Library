d0b79e73a0c943acca9bbc72b782785d1761ca0b
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-06 12:38:54 +0530

    07-06-2026 1911-maximum-alternating-subsequence-sum Medium Array

---
+++ LeetCode/Medium/1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    typedef long long ll;
+    ll t[1000001][2];
+    ll solve(int idx , vector<int>& nums , bool isEven){
+
+        if(idx >= nums.size()) return 0;
+
+        if(t[idx][isEven] != -1){
+            return t[idx][isEven];
+        }
+
+        ll skip = solve(idx+1 , nums , isEven);
+
+        ll val = nums[idx];
+
+        if(isEven == false){
+            val = -val;
+        }
+
+        ll take = solve(idx+1 , nums , !isEven) + val;
+
+        return t[idx][isEven]= max(skip , take);
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(0 ,nums ,  true);// 0 : even index , true : + 
+    }
+};
