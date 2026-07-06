ec73512d764d64da22509bee1919fdb2c7a46a92
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-06 13:55:47 +0530

    Update 1911-maximum-alternating-subsequence-sum.cpp

--- LeetCode/Medium/1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ LeetCode/Medium/1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -60,3 +60,50 @@ public:
 };
 //T.C : O(2n)
 //s.C : O(n)
+
+//Approach3: bottom up 
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<long>> t(n, vector<long>(2, 0));
+
+        t[0][0] = max(-nums[0], 0);
+        t[0][1] = max(nums[0], 0);
+            
+        for(int i = 1; i < n; i++) {
+            
+            t[i][0] = max(t[i-1][1] - nums[i], t[i-1][0]);
+            
+            t[i][1] = max(t[i-1][0] + nums[i], t[i-1][1]);
+            
+            
+        }
+        
+        return max(t[n-1][0], t[n-1][1]);
+    }
+};
+
+//Approach- (Same as Approach-3 - Only difference is that I have used 1 based indexing to make it general to other Botto Up)
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<long>> t(n+1, vector<long>(2, 0));
+            
+        for(int i = 1; i < n+1; i++) {
+            
+            t[i][0] = max(t[i-1][1] - nums[i-1], t[i-1][0]);
+            
+            t[i][1] = max(t[i-1][0] + nums[i-1], t[i-1][1]);
+            
+            
+        }
+        
+        return max(t[n][0], t[n][1]);
+    }
+};
+//T.C : O(n)
+//S.C : O(n)
