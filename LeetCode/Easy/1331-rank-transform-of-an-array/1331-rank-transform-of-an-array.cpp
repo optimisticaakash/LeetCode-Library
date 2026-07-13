fa0878a8632ad821e32584e633027ff17661f6b9
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-13 14:52:06 +0530

    Update 1331-rank-transform-of-an-array.cpp

--- LeetCode/Easy/1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ LeetCode/Easy/1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,12 +1,16 @@
+//Approach1: my approach 
+//1. ek temp array me arr ko dal kr sort krenge
+//2. sorted array ke hisab se har no , rank ko map me store krenge
+//3. arr pr iterate krke mp se number ke corresponding rank ko dal denge
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         vector<int> temp = arr;
-       sort(temp.begin() , temp.end());
+       sort(temp.begin() , temp.end()); //nlogn
 
-       unordered_map<int,int> rankmp;
+       unordered_map<int,int> rankmp;//n
 
-       for(int i = 0; i < arr.size(); i++){
+       for(int i = 0; i < arr.size(); i++){//O(n
             if(rankmp.find(temp[i]) == rankmp.end()){
                 int size = rankmp.size();
                 rankmp[temp[i]] = size+1;
@@ -14,10 +18,13 @@ public:
        }
 
        vector<int> ans;
-       for(int i = 0; i < arr.size() ; i++){
+       for(int i = 0; i < arr.size() ; i++){//O(n)
             ans.push_back(rankmp[arr[i]]);
        }
 
        return ans;
     }
 };
+
+//T.C : O(2n) + O(nlogn)
+//S.C : O(2n)
