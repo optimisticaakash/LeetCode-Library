0f83e4fe2d4d89816a8541e91856e496111fbb21
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-19 20:24:55 +0530

    Update 0316-remove-duplicate-letters.cpp

--- LeetCode/Medium/0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ LeetCode/Medium/0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,3 +1,43 @@
+//Approach1: using string as stack
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+
+        vector<int>LastIndex(26,0);
+        for(int i = 0; i < n; i++){
+            char ch = s[i];
+
+            LastIndex[ch-'a'] = i;
+        }
+
+        vector<bool>taken(26,false);
+
+        string result = "";
+
+        for(int i = 0; i < n; i++){
+            char ch = s[i];
+            
+            int idx = ch-'a';
+
+            if(taken[idx] == true) continue;
+
+            while(result.length() > 0 && result.back() > ch && LastIndex[result.back()-'a'] > i){
+                taken[result.back()-'a'] = false;
+                result.pop_back();
+            }
+
+            result.push_back(ch);
+            taken[idx] = true;
+        }
+
+        return result;
+    }
+};
+//T.C : O(N)
+//S.C : O(1)
+
+//Approach2: using stack
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
@@ -42,3 +82,6 @@ public:
         return result;
     }
 };
+
+//T.C : O(N)
+//S.C : O(1)
