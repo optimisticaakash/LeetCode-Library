ef7d1c55988574dc27b7c0a3d14a7f5387b4b6c1
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-31 23:01:28 +0530

    07-31-2026 3016-minimum-number-of-pushes-to-type-word-ii Medium Hash Table

---
+++ LeetCode/Medium/3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+       int n = word.size();
+
+        vector<int> mp(26,0);
+
+        for(char &ch : word){
+            mp[ch-'a']++;
+        }
+
+        sort(mp.begin() , mp.end() , greater<int>()); //descending
+
+        int result = 0;
+        for(int i = 0; i < 26; i++){
+            int freq = mp[i];
+
+            int press = i/8+1;
+
+            result += (press *freq);
+        }
+
+        return result; 
+    }
+};
