bf2896f0c32f3909c373c9a64243d31a892edf84
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-07-31 22:45:35 +0530

    07-31-2026 3014-minimum-number-of-pushes-to-type-word-i Easy Math

--- LeetCode/Easy/3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ LeetCode/Easy/3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        if(word.length() <= 8) {
-            return word.length();
-        }
-        
-        int count = 0;
-        unordered_map<int, int> mp; //You can use an array of size 10 also and use index 2 to 9
-        
-        int assign = 2;
-        for(char &ch : word) {
-            
-            if(assign > 9) {
-                assign = 2;
+        int result = 0;
+
+        unordered_map<int,int> mp; 
+
+        int assign_key = 2;
+
+        for(char &ch : word){
+
+            if(assign_key > 9){
+                assign_key = 2;
             }
-            
-            mp[assign]++;
-            count += mp[assign];
-            assign++;
+
+            mp[assign_key]++;
+            result += mp[assign_key];
+
+            assign_key++;
         }
-        
-        return count;
-        
+
+        return result;
     }
 };
