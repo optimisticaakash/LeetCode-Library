4d3cc1df0b095d25eb972bd7711e25964981da8a
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-08-05 14:29:34 +0530

    08-05-2026 0486-predict-the-winner Medium Array

--- LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
+++ LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,6 +1,6 @@
-//Approach1: recursion
 class Solution {
 public:
+    int t[22][22];
     int solve(int i , int j , vector<int>& nums){
         //base caase: koi element nhi bacha
         if(i > j){
@@ -9,13 +9,18 @@ public:
 
         if(i == j) return nums[i]; //sirf ek hi element;
 
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
         int take_i = nums[i] + min(solve(i+2 , j , nums) , solve(i+1, j-1 , nums));
 
         int take_j = nums[j] + min(solve(i+1 , j-1 , nums) , solve(i, j-2 , nums));
 
-        return max(take_i , take_j);
+        return t[i][j] =  max(take_i , take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
+        memset(t,-1,sizeof(t));
         int n = nums.size();
 
         int total_score = accumulate(begin(nums) , end(nums) , 0);
@@ -26,5 +31,3 @@ public:
         return player1_score >= player2_score;
     }
 };
-//T.C : O(2^n)
-//S.C : O(N)
