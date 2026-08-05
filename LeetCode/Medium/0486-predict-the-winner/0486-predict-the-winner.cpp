aa678fbb1a4c17048778925f794d4501b1cff907
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-08-05 14:31:58 +0530

    Update 0486-predict-the-winner.cpp

--- LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
+++ LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,3 +1,36 @@
+//Approach1 : recursion
+class Solution {
+public:
+    int solve(int i , int j , vector<int>& nums){
+        //base caase: koi element nhi bacha
+        if(i > j){
+            return 0;
+        }
+
+        if(i == j) return nums[i]; //sirf ek hi element;
+
+        int take_i = nums[i] + min(solve(i+2 , j , nums) , solve(i+1, j-1 , nums));
+
+        int take_j = nums[j] + min(solve(i+1 , j-1 , nums) , solve(i, j-2 , nums));
+
+        return max(take_i , take_j);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+
+        int total_score = accumulate(begin(nums) , end(nums) , 0);
+
+        int player1_score = solve(0 , n-1 , nums);
+        int player2_score = total_score - player1_score;
+
+        return player1_score >= player2_score;
+    }
+};
+//T.C : O(2^n)
+//s.C : O(n)
+
+
+//Approach2 : recursion + memoization
 class Solution {
 public:
     int t[22][22];
@@ -31,3 +64,5 @@ public:
         return player1_score >= player2_score;
     }
 };
+//T.C : O(n^2)
+//S.C : : O(n^2)
