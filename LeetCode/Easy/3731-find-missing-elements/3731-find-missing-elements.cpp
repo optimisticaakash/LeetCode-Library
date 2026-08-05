d319d2cd2b5d64ee170bb2c3e82e4df6f5ff212f
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-08-05 15:16:38 +0530

    Update 3731-find-missing-elements.cpp

--- LeetCode/Easy/3731-find-missing-elements/3731-find-missing-elements.cpp
+++ LeetCode/Easy/3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,3 +1,4 @@
+//Approach1:
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
@@ -21,3 +22,32 @@ public:
         return result;
     }
 };
+//T.C : O(nlogn + T) T : no of element between minEl and maxEl
+//S.C : O(K) k : no of missing elements
+
+//Approach2:
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+
+        int n = nums.size();
+
+        vector<int> ans;
+
+        for(int i = 1; i < n; i++){
+            int prev = nums[i-1];
+            int curr = nums[i];
+
+            //add all number between prev and curr jo bhi missing hai 
+            for(int j = prev+1; j < curr ; j++){
+                ans.push_back(j);
+            }
+        }
+
+        return ans;
+    }
+};
+
+//T.C : O(nlogn + T) T : no of element between minEl and maxEl
+//S.C : O(K) k : no of missing elements
