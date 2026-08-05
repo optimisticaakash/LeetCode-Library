21271537e490af3fa323088e750384ae2c34e2af
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-08-05 15:22:16 +0530

    08-05-2026 3731-find-missing-elements Easy Array

--- LeetCode/Easy/3731-find-missing-elements/3731-find-missing-elements.cpp
+++ LeetCode/Easy/3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,53 +1,27 @@
-//Approach1:
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
         int n = nums.size();
 
-        sort(begin(nums) , end(nums));
+        vector<bool> present(101,false);
 
-        int curr = nums[0];
-        vector<int> result;
+        int mini = nums[0];
+        int maxi = nums[0];
+        for(int i = 0; i < n; i++){
+            maxi = max(nums[i] , maxi);
+            mini = min(nums[i] , mini);
 
-        for(int i = 0; i < n;){
-            if(curr < nums[i]){
-                //missing curr
+            present[nums[i]] = true;
+        }
+
+        vector<int>result;
+
+        for(int curr = mini; curr < maxi; curr++){
+            if(present[curr] != true){
                 result.push_back(curr);
-            }else{
-                i++;
             }
-            curr++;
         }
 
         return result;
     }
 };
-//T.C : O(nlogn + T) T : no of element between minEl and maxEl
-//S.C : O(K) k : no of missing elements
-
-//Approach2:
-class Solution {
-public:
-    vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-
-        int n = nums.size();
-
-        vector<int> ans;
-
-        for(int i = 1; i < n; i++){
-            int prev = nums[i-1];
-            int curr = nums[i];
-
-            //add all number between prev and curr jo bhi missing hai 
-            for(int j = prev+1; j < curr ; j++){
-                ans.push_back(j);
-            }
-        }
-
-        return ans;
-    }
-};
-
-//T.C : O(nlogn + T) T : no of element between minEl and maxEl
-//S.C : O(K) k : no of missing elements
