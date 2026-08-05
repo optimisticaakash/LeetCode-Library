5a8811f4d75d35d68a3e4d008f2cc81a075180eb
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-08-05 14:44:22 +0530

    08-05-2026 0486-predict-the-winner Medium Array

--- LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
+++ LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,68 +1,23 @@
-//Approach1 : recursion
-class Solution {
-public:
-    int solve(int i , int j , vector<int>& nums){
-        //base caase: koi element nhi bacha
-        if(i > j){
-            return 0;
-        }
-
-        if(i == j) return nums[i]; //sirf ek hi element;
-
-        int take_i = nums[i] + min(solve(i+2 , j , nums) , solve(i+1, j-1 , nums));
-
-        int take_j = nums[j] + min(solve(i+1 , j-1 , nums) , solve(i, j-2 , nums));
-
-        return max(take_i , take_j);
-    }
-    bool predictTheWinner(vector<int>& nums) {
-        int n = nums.size();
-
-        int total_score = accumulate(begin(nums) , end(nums) , 0);
-
-        int player1_score = solve(0 , n-1 , nums);
-        int player2_score = total_score - player1_score;
-
-        return player1_score >= player2_score;
-    }
-};
-//T.C : O(2^n)
-//s.C : O(n)
-
-
-//Approach2 : recursion + memoization
 class Solution {
 public:
     int t[22][22];
     int solve(int i , int j , vector<int>& nums){
-        //base caase: koi element nhi bacha
-        if(i > j){
-            return 0;
-        }
+        if(i > j) return 0;
 
-        if(i == j) return nums[i]; //sirf ek hi element;
+        if(i == j) return nums[i];
 
         if(t[i][j] != -1){
             return t[i][j];
         }
 
-        int take_i = nums[i] + min(solve(i+2 , j , nums) , solve(i+1, j-1 , nums));
-
-        int take_j = nums[j] + min(solve(i+1 , j-1 , nums) , solve(i, j-2 , nums));
+        int take_i = nums[i] - solve(i+1 , j , nums);
+        int take_j = nums[j] - solve(i , j-1 , nums);
 
         return t[i][j] =  max(take_i , take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
         int n = nums.size();
-
-        int total_score = accumulate(begin(nums) , end(nums) , 0);
-
-        int player1_score = solve(0 , n-1 , nums);
-        int player2_score = total_score - player1_score;
-
-        return player1_score >= player2_score;
+        memset(t,-1,sizeof(t));
+        return solve(0 , n-1 , nums) >= 0;
     }
 };
-//T.C : O(n^2)
-//S.C : : O(n^2)
