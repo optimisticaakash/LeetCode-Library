0f5b0da2e92777a5613157051e0c8242ccebe795
optimist_aakash <130359837+optimisticaakash@users.noreply.github.com>
2026-09-05 13:34:09 +0530

    09-05-2026 0704-binary-search Easy Array

--- LeetCode/Easy/0704-binary-search/0704-binary-search.cpp
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.cpp
@@ -1,21 +1,15 @@
-//iterative 
 class Solution {
 public:
+    int recursiveBS(vector<int>& nums , int low , int high , int target){
+        if(low > high) return -1;
+
+        int mid = low + (high - low)/2;
+
+        if(nums[mid] == target) return mid;
+        else if(target > nums[mid]) return recursiveBS(nums , mid+1 , high , target);
+        else return  recursiveBS(nums , low , mid-1 , target);
+    }
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        int low =0;
-        int high = n-1;
-
-        while(low <= high){
-            int mid = low + (high-low)/2;
-
-            if(nums[mid] == target) return mid;
-            else if(target > nums[mid]) low = mid+1;
-            else high = mid-1;
-        }
-
-
-        return -1;
+        return recursiveBS(nums , 0 , nums.size() -1 , target);
     }
 };
